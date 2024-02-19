08c2e644906ac2b4a7edba191b6bd6d10cd7b202
xshujo <xshujo@gmail.com>
2024-02-19 12:49:31 -0500

    Category.js styles

--- betterment/src/components/Category.js
+++ betterment/src/components/Category.js
@@ -1,12 +1,46 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View, Platform } from "react-native";
 
 export default function Category({ title, value, summary }) {
     return (
-        <View>
-            <Text>{title}</Text>
-            <Text>{value}</Text>
-            <Text>{summary}</Text>
+        <View style={styles.section}>
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.value}>{value}</Text>
+            <Text style={styles.summary}>{summary}</Text>
         </View>
     );
 }
+
+const { width, height } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+    section: {
+        width: width * 0.9,
+        backgroundColor: 'white',
+        borderRadius: 16,
+        padding: 16,
+        ...Platform.select({
+            ios: {
+                shadowColor: '#30200d',
+                shadowOffset: { width: 0, height: 2 },
+                shadowOpacity: 0.3,
+                shadowRadius: 1,
+            },
+            android: {
+                elevation: 1,
+            },
+        }),
+    },
+    title: {
+        fontSize: 22,
+        marginBottom: 12
+    },
+    value: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        marginBottom: 6
+    },
+    summary: {
+        fontSize: 16
+    }
+});
