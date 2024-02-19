1f5a8d7084908c324d7379cf44a5b9d820a1df22
xshujo <xshujo@gmail.com>
2024-02-19 12:09:04 -0500

    Header.js styles

--- betterment/App.js
+++ betterment/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import JournalScreen from './src/screens/JournalScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>
-        Hello World
-      </Text>
       {/* Temp */}
       <HomeScreen />
-      <JournalScreen />
+      {/* <JournalScreen /> */}
     </View>
   );
 }
 
+const { width, height } = Dimensions.get('window');
+
 const styles = StyleSheet.create({
   container: {
+    width: width,
+    height: height,
     backgroundColor: '#f3e7d8',
     flex: 1,
     justifyContent: 'center',
