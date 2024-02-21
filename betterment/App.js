1a1983ee1a20f71595cbdc63ca588bd504038950
xshujo <xshujo@gmail.com>
2024-02-21 14:24:46 -0500

    Clean up App.js

--- betterment/App.js
+++ betterment/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, View, Text, Dimensions } from 'react-native';
+
 import HomeScreen from './src/screens/HomeScreen';
 import LogScreen from './src/screens/LogScreen';
 import JournalScreen from './src/screens/JournalScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 import Footer from './src/components/Footer';
+
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -24,23 +25,5 @@ export default function App() {
       </Stack.Navigator>
       <Footer />
     </NavigationContainer>
-    // <View style={styles.container}>
-    //   <HomeScreen />
-    //   <JournalScreen />
-    //   <Footer />
-    // </View>
   );
 }
-
-const { width, height } = Dimensions.get('window');
-
-const styles = StyleSheet.create({
-  container: {
-    width: width,
-    height: height,
-    backgroundColor: '#f3e7d8',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
