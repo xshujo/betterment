8ece929c1410d7b2ea52ccfa2564792fb41ef221
xshujo <xshujo@gmail.com>
2024-03-04 12:29:21 -0500

    Navigation stopped working; code cleanup

--- betterment-v2/screens/HomeScreen.js
+++ betterment-v2/screens/HomeScreen.js
@@ -2,12 +2,11 @@
 
 import React from "react";
 import { Text, View } from "react-native";
-import Header from "../components/Header";
 
 export default function HomeScreen() {
-	return (
-		<View>
-			<Text>Home</Text>
-		</View>
-	);
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
 }
--- betterment-v2/screens/ProfileScreen.js
+++ betterment-v2/screens/ProfileScreen.js
@@ -1,13 +1,12 @@
 // This component is a screen that displays the user's informations and allows them to set their goals //
 
 import React from "react";
-import { View } from "react-native";
-import Header from "../components/Header";
+import { Text, View } from "react-native";
 
 export default function ProfileScreen() {
-	return (
-		<View>
-			<Header screen="Profile" />
-		</View>
-	);
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
 }
