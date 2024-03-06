35d7e09e8253c9fd03ac60ed31ce1bb7e0365c6a
xshujo <xshujo@gmail.com>
2024-03-06 15:50:23 -0500

    New icons for header

--- betterment-v2/components/Header.js
+++ betterment-v2/components/Header.js
@@ -2,47 +2,47 @@
 
 import React from "react";
 
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 import { useCurrentScreen } from "../hooks/useCurrentScreen";
 import { Colors } from "../constants/Colors";
 
-import GoBackIcon from "../assets/images/icon.png";
-import SettingsIcon from "../assets/images/icon.png";
 import { useNavigation, useRoute } from "@react-navigation/core";
+import { GoBackIcon, SettingsIcon } from "./SvgIcon";
 
 export default function Header() {
   const currentScreen = useCurrentScreen();
 
-  const navigation = useNavigation(); // Initialize navigation
+  const navigation = useNavigation();
   const route = useRoute();
 
   const handleGoBack = () => {
-    navigation.navigate('Home'); // Navigate to the home screen
+    navigation.navigate('Home');
   };
 
   const renderGoBackButton = () => {
-    if (route.name !== 'Home') { // Check if the current screen is not the home screen
+    if (route.name !== 'Home') {
       return (
         <TouchableOpacity style={styles.iconContainer} onPress={handleGoBack}>
-          <Image source={GoBackIcon} style={{ width: 16, height: 16, }} />
+          <GoBackIcon />
         </TouchableOpacity>
       );
     } else {
-      return <View style={{ width: 80 }} />; // Render an empty View component if on the home screen
+      return <View style={{ width: 80 }} />;
     }
   };
 
   return (
     <View style={styles.container}>
-      {renderGoBackButton()}{/* Render the "Go back" button */}
+      {/* Go back button */}
+      {renderGoBackButton()}
 
       {/* Screen name */}
       <Text style={styles.screenNameText}>{currentScreen}</Text>
 
       {/* Settings button */}
       <TouchableOpacity style={styles.iconContainer}>
-        <Image source={SettingsIcon} style={{ width: 16, height: 16, }} />
+        <SettingsIcon />
       </TouchableOpacity>
     </View>
   );
