0ac68c775aa5d705b3a333cd7f74ee0e8f4e9278
xshujo <xshujo@gmail.com>
2024-03-07 14:11:56 -0500

    Input style (rounded borders)

--- betterment-v2/components/Input.js
+++ betterment-v2/components/Input.js
@@ -3,14 +3,10 @@ import { StyleSheet, Text, TextInput, View } from "react-native";
 import { Colors } from "../constants/Colors";
 
 export default function Input({ inputName, unit, charLimit }) {
-  const weightUnit = ["kg", "lbs"];
-  const heightUnit = ["cm", "ft"];
-  const measurementUnit = ["cm, in"];
-
   return (
     <View style={styles.categoryItem}>
       <Text style={styles.h3}>{inputName}</Text>
-      <View style={styles.goalFlex}>
+      <View style={styles.inputContainer}>
         <TextInput style={styles.textInput} keyboardType='numeric' placeholder='Value' maxLength={charLimit} />
         <View>
           <Text style={styles.h3}>{unit}</Text>
@@ -24,34 +20,24 @@ const styles = StyleSheet.create({
   h3: {
     fontFamily: 'Nunito-Bold'
   },
-  categoryContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    rowGap: 20,
-  },
   categoryItem: {
     gap: 4,
   },
-  goalFlex: {
+  inputContainer: {
     width: 144,
     backgroundColor: Colors.primaryLight,
+    borderRadius: 16,
     flexDirection: 'row',
     alignItems: 'center',
     gap: 16,
-  },
-  multiInputContainer: {
-    width: 144,
-    backgroundColor: Colors.primary,
-    flexDirection: 'row',
-    zIndex: 5,
   },
   textInput: {
     width: 72,
     backgroundColor: Colors.accent1,
     fontFamily: 'Nunito-Regular',
     borderColor: Colors.primary,
+    borderTopLeftRadius: 16,
+    borderBottomLeftRadius: 16,
     borderWidth: 1,
     paddingVertical: 8,
     paddingLeft: 16,
