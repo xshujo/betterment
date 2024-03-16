d2b661225e044b00b28cf9805a408c3828edbeeb
Taïsha DM <xshujo@gmail.com>
2024-03-16 16:40:01 -0400

    Temp content in home screen

--- betterment-v2/screens/HomeScreen.js
+++ betterment-v2/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 // This component is the home screen. It displays the user's stats of the day or any chosen day //
 
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from 'expo-linear-gradient';
 
 import { Colors } from "../constants/Colors";
@@ -10,63 +10,73 @@ import ProgressBar from "../components/ProgressBar";
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      {/* Top stat */}
-      <View style={styles.progressContainer}>
-        <View style={styles.progressSubContainer}>
-          {/* The progress */}
-          <ProgressBar radius={100} strokeWidth={16} progress={0.7} />
-          {/* The stat */}
-          <View style={styles.currentTextContainer}>
-            <Text style={styles.h1}>8750</Text>
-            <Text style={styles.h2}>steps</Text>
+      <ScrollView contentContainerStyle={{ justifyContent: 'center', gap: 24 }}>
+        <Text style={styles.tempText}>This page will eventually display your progress based on your goals set in the Profile page, and your Log entries. Here's a sneak peak.</Text>
+        <Text style={styles.tempText}>x</Text>
+        <Text style={styles.tempText}>Cette page affichera éventuellement vos progrès en fonction de vos objectifs définis dans la page Profil et de vos entrées de journal (Log). Voici un aperçu.</Text>
+        {/* Top stat */}
+        <View style={styles.progressContainer}>
+          <View style={styles.progressSubContainer}>
+            {/* The progress */}
+            <ProgressBar radius={100} strokeWidth={16} progress={0.7} />
+            {/* The stat */}
+            <View style={styles.currentTextContainer}>
+              <Text style={styles.h1}>8750</Text>
+              <Text style={styles.h2}>steps</Text>
+            </View>
           </View>
+          <Text style={styles.h4}>1250 steps remaining</Text>
         </View>
-        <Text style={styles.h4}>1250 steps remaining</Text>
-      </View>
-      {/* Stats */}
-      <View style={styles.categoriesContainer}>
-        <LinearGradient colors={['#FED3D4', '#FED3D4', '#EEFFF3']}
-          start={[0, 1]}
-          end={[1, 0]}
-          style={styles.gradient}>
-          <View style={styles.categoryContainer}>
-            <Text style={styles.h3}>Activity</Text>
-            <Text>Stats</Text>
-          </View>
-        </LinearGradient>
-        <LinearGradient colors={['#FED3D4', '#FED3D4', '#EEFFF3']}
-          start={[0, 1]}
-          end={[1, 0]}
-          style={styles.gradient}>
-          <View style={styles.categoryContainer}>
-            <Text style={styles.h3}>Calorie intake*</Text>
-            <Text>Stats</Text>
-          </View>
-        </LinearGradient>
-        <LinearGradient colors={['#FED3D4', '#FED3D4', '#EEFFF3']}
-          start={[0, 1]}
-          end={[1, 0]}
-          style={styles.gradient}>
-          <View style={styles.categoryContainer}>
-            <Text style={styles.h3}>Sleep</Text>
-            <Text>Stats</Text>
-          </View>
-        </LinearGradient>
-        <LinearGradient colors={['#FED3D4', '#FED3D4', '#EEFFF3']}
-          start={[0, 1]}
-          end={[1, 0]}
-          style={styles.gradient}>
-          <View style={styles.categoryContainer}>
-            <Text style={styles.h3}>Activity</Text>
-            <Text>Stats</Text>
-          </View>
-        </LinearGradient>
-      </View>
+        {/* Stats */}
+        <View style={styles.categoriesContainer}>
+          <LinearGradient colors={['#FED3D4', '#FED3D4', '#EEFFF3']}
+            start={[0, 1]}
+            end={[1, 0]}
+            style={styles.gradient}>
+            <View style={styles.categoryContainer}>
+              <Text style={styles.h3}>Activity</Text>
+              <Text>Stats</Text>
+            </View>
+          </LinearGradient>
+          <LinearGradient colors={['#FED3D4', '#FED3D4', '#EEFFF3']}
+            start={[0, 1]}
+            end={[1, 0]}
+            style={styles.gradient}>
+            <View style={styles.categoryContainer}>
+              <Text style={styles.h3}>Calorie intake</Text>
+              <Text>Stats</Text>
+            </View>
+          </LinearGradient>
+          <LinearGradient colors={['#FED3D4', '#FED3D4', '#EEFFF3']}
+            start={[0, 1]}
+            end={[1, 0]}
+            style={styles.gradient}>
+            <View style={styles.categoryContainer}>
+              <Text style={styles.h3}>Sleep</Text>
+              <Text>Stats</Text>
+            </View>
+          </LinearGradient>
+          <LinearGradient colors={['#FED3D4', '#FED3D4', '#EEFFF3']}
+            start={[0, 1]}
+            end={[1, 0]}
+            style={styles.gradient}>
+            <View style={styles.categoryContainer}>
+              <Text style={styles.h3}>Weight</Text>
+              <Text>Stats</Text>
+            </View>
+          </LinearGradient>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  tempText: {
+    fontFamily: 'Nunito-Medium',
+    fontSize: 16,
+    textAlign: 'center',
+  },
   container: {
     backgroundColor: Colors.accent1,
     padding: 24,
@@ -88,7 +98,8 @@ const styles = StyleSheet.create({
   },
   h3: {
     fontFamily: 'Nunito-Bold',
-    fontSize: 18,
+    fontSize: 16,
+    textAlign: 'center',
     textTransform: 'uppercase',
   },
   h4: {
